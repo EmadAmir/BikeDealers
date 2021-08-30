42dcb0a3099ade8b2fe87806dd6daeef2638b515
Emad <24242.imad@gmail.com>
2021-08-30 17:02:18 +0100

    Applied Code first approach developed two tables Make and model

---
+++ BikeDealers/BikeDealers/AppDbContext/BikeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BikeDealers.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeDealers.AppDbContext
+{
+    public class BikeDbContext:DbContext
+    {
+        /*
+        Reference Links
+        https://entityframeworkcore.com/knowledge-base/49736557/implementing-dbcontextoptions
+        https://www.tektutorialshub.com/entity-framework-core/ef-core-dbcontext/
+        */
+        public BikeDbContext(DbContextOptions<BikeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models{ get; set; }
+    }
+}
